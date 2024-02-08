23248f11886274626ee7de2fba491209daa9733e
Lyndon <lhughey@its-training.com>
2024-02-08 04:56:39 -0600

    init

---
+++ MauiPipelineTest/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiPipelineTest;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ MauiPipelineTest/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiPipelineTest;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
